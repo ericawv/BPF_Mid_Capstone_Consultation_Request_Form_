5a87c8690156b47f1bab35b76ec9b4066034b666
Erica V <92328917+ericawv@users.noreply.github.com>
2022-01-07 18:39:33 -0500

    Commit of the Consultation Request Form Capstone files
    Commit of the Consultation Request Form Capstone files

--- server/server.js
+++ server/server.js
@@ -1,30 +1,31 @@
-require("dotenv").config()
+require("dotenv").config()  //Needed for when the database is incorporated
 
-const express = require("express")
+const express = require("express") //Framework imported for server setup to respond to HTTP request
 const cors = require("cors")
 
-const app = express()
+const app = express() //Needed for setting up the server 
 
-const { SERVER_PORT } = process.env
+const { SERVER_PORT } = process.env  //Needed for setting up the port using a .env file
 
-app.use(express.json())
+app.use(express.json()) //Needed to be able to accept JSON
 app.use(cors())
 
 
-
+//Using the GET API, endpoint sends back the index of the requested Available Consultation Dates to the frontl-end.
 app.get("/api/availableConsultDates", (req, res) => {
     const availableConsultDates = ["December 4, 2021", "December 11, 2021", "December 18, 2021"]
     let reply = availableConsultDates.join('\r\n')
     res.status(200).send(reply)
 })
 
-
+//Using the POST API, endpoint sends back the confirmation message that includes the date requested using a template literal string
 app.post("/api/dateRequest", (req, res) => {
-    console.log(req.body)
+   // console.log(req.body)
     let {post} = req.body
     res.status(200).send(`Thank you! Your date selection of, ${post} has been scheduled.`)
 })
 
+//Defining the port that the server will use to run
 const port = process.env.PORT || 4747
 app.listen(port, () =>  console.log(`Server is running on port: ${port}`))
 
