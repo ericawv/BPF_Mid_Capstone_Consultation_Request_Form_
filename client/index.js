f8c93b520c4dc16889fd6272bbdf7451f62319c4
Erica V <92328917+ericawv@users.noreply.github.com>
2022-01-07 18:37:40 -0500

    Commit of the Consultation Request Form Capstone files
    Commit of the Consultation Request Form Capstone files

--- client/index.js
+++ client/index.js
@@ -1,26 +1,27 @@
 // const { default: axios } = require("axios")
 
-//Comment
+//Prevents the form input data from being cleared when the screen refreshes before the submit action takes place
   document.querySelector("form").addEventListener('submit', (event) => {
     event.preventDefault()
   })
 
-//Comment  
+//When the View Available Dates button is clicked. Using the GET API, the Available Consultation Dates are returned in an alert box  
 document.getElementById("availableConsultDates").onclick = function () {
   axios.get("http://localhost:4747/api/availableConsultDates/")
        .then(function (response) {
         const data = response.data;
-        console.log(data);
+        // console.log(data);
         alert(data);
     });
 };
 
+//When the Book button is clicked. The entered date input value is posted to the server
 document.getElementById("bookAppointment").onclick = function () {
         let dateInput = document.getElementById("dateRequest")  
         let enteredDate = {
         post: dateInput.value  
     }
-    
+//Using the POST API, to return the booked consultation confirmation with the date entered/posted in an alert box. Form resets once the alert box is respondeds to.     
     axios.post("http://localhost:4747/api/dateRequest/", enteredDate)
          .then(function (response) {
           const data = response.data
@@ -28,6 +29,8 @@ document.getElementById("bookAppointment").onclick = function () {
           document.querySelector("form").reset() 
         })
   }
+
+//Following funcations styles each form input field and the select box to change from transparent to a white background once a value is entered and selected.    
 
 function focusFunction() {
   document.getElementById("input").style.background = "white";
@@ -45,24 +48,25 @@ function blurFunctionLast() {
   document.getElementById("inputLast").style.background = "white";
 }
 function focusFunctionPhone() {
-    document.getElementById("inputPhone").style.background = "white";
+  document.getElementById("inputPhone").style.background = "white";
   }
   
-  function blurFunctionPhone() {
-    document.getElementById("inputPhone").style.background = "white";
+function blurFunctionPhone() {
+  document.getElementById("inputPhone").style.background = "white";
   }
 
 function focusFunctionDate() {
-    document.getElementById("dateRequest").style.background = "white";
+  document.getElementById("dateRequest").style.background = "white";
   }
-  function blurFunctionDate() {
-    document.getElementById("dateRequest").style.background = "white";
+  
+function blurFunctionDate() {
+  document.getElementById("dateRequest").style.background = "white";
   }
 
 function focusFunctionSelect() {
-    document.getElementById("inputSelect").style.background = "white";
+  document.getElementById("inputSelect").style.background = "white";
   }
   
-  function blurFunctionSelect() {
-    document.getElementById("inputSelect").style.background = "white";
+function blurFunctionSelect() {
+  document.getElementById("inputSelect").style.background = "white";
   }
